6a95c681ff7ecdaee8e07f5dcec9fd3593849c07
Terje Moe <terjemoe54@hotmail.com>
2023-03-24 08:44:07 +0100

    Initial Commit

---
+++ LerningGH/LerningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LerningGHApp.swift
+//  LerningGH
+//
+//  Created by Terje Moe on 24/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct LerningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
